9f17174d4fc8082696a5829799573ed6d609dc76
jacek <jacekpazera@wp.pl>
2019-09-24 06:53:33 +0200

    [+] CurrentSection. new Read & Write (color, html color, font styles...)

--- source/JPP.MemIniFile.pas
+++ source/JPP.MemIniFile.pas
@@ -1,4 +1,6 @@
-unit JPP.MemIniFile;
+unit JPP.MemIniFile deprecated 'Use JPL.MemIniFile instead';
+
+
 
 {$IFDEF FPC} {$mode objfpc}{$H+} {$ENDIF}
 
@@ -7,11 +9,11 @@ interface
 uses
   {$IFDEF DCC}
   Winapi.Windows,
-  System.SysUtils, System.Classes, System.IniFiles, System.ZLib, Vcl.Graphics,  Vcl.Dialogs,
+  System.SysUtils, System.Classes, System.IniFiles, System.ZLib, Vcl.Graphics, Vcl.Dialogs,
   {$ELSE}
   SysUtils, Classes, IniFiles, Graphics,
   {$ENDIF}
-  JPL.Strings,
+  JPL.Strings, JPL.Colors, JPL.Math, JPL.Conversion,
   JPP.Common.Procs
   ;
 
@@ -21,34 +23,68 @@ type
 
   {$region ' ---------------------- INT - TJppMemIniFile ----------------------- '}
   TJppMemIniFile = class(TMemIniFile)
+  private const
+    DEFAULT_SECTION = 'MAIN';
   private
     FLeftStringBound: string;
     FRightStringBound: string;
+    FCurrentSection: string;
     procedure SetLeftStringBound(const Value: string);
     procedure SetRightStringBound(const Value: string);
+    procedure SetCurrentSection(const Value: string);
+    function GetCurrentSection: string;
+    procedure CheckCurrentSection;
   public
+
     constructor Create(const AFileName: string); overload;
     constructor Create(const AFileName: string; const AEncoding: TEncoding); overload;
-    procedure WriteColor(const Section: string; const Ident: string; Color: TColor);
-    function ReadColor(const Section: string; const Ident: string; Default: TColor): TColor;
-    procedure WriteFontStyle(const Section: string; const Ident: string; FontStyles: TFontStyles);
-    function ReadFontStyle(const Section: string; const Ident: string; Default: TFontStyles): TFontStyles;
+
+
+    procedure WriteColor(const Section: string; const Ident: string; const Color: TColor); overload;
+    procedure WriteColor(const Ident: string; const Color: TColor); overload;
+    function ReadColor(const Section: string; const Ident: string; const Default: TColor): TColor; overload;
+    function ReadColor(const Ident: string; const Default: TColor): TColor; overload;
+
+
+    procedure WriteHtmlColor(const Section: string; const Ident: string; const AColor: TColor); overload;
+    procedure WriteHtmlColor(const Ident: string; const AColor: TColor); overload;
+    function ReadHtmlColor(const Section, Ident: string; const Default: TColor): TColor; overload;
+    function ReadHtmlColor(const Ident: string; const Default: TColor): TColor; overload;
+
+    function ReadIntegerInRange(const Section, Ident: string; const Default, Min, Max: integer): integer; overload;
+    function ReadIntegerInRange(const Ident: string; const Default, Min, Max: integer): integer; overload;
+
+
+    procedure WriteFontStyle(const Section: string; const Ident: string; FontStyles: TFontStyles); overload;
+    procedure WriteFontStyle(const Ident: string; FontStyles: TFontStyles); overload;
+    function ReadFontStyle(const Section: string; const Ident: string; Default: TFontStyles): TFontStyles; overload;
+    function ReadFontStyle(const Ident: string; Default: TFontStyles): TFontStyles; overload;
 
     procedure WriteStrings(const Section: string; Items: TStrings {$IFDEF DCC}; Compress: Boolean = False{$ENDIF});
-
     procedure ReadStrings(const Section: string; Items: TStrings {$IFDEF DCC}; ItemsCompressed: Boolean = False{$ENDIF});
 
-    procedure WriteBoundString(const Section, Ident, Value: string);
-    function ReadBoundString(const Section, Ident, Default: string): string;
+    procedure WriteBoundString(const Section, Ident, Value: string); overload;
+    procedure WriteBoundString(const Ident, Value: string); overload;
+    function ReadBoundString(const Section, Ident, Default: string): string; overload;
+    function ReadBoundString(const Ident, Default: string): string; overload;
+
     {$IFDEF DCC}
-    procedure WriteInt64(const Section, Ident: string; const Value: Int64);
-    function ReadInt64(const Section, Ident: string; const Default: Int64): Int64;
+    procedure WriteInt64(const Section, Ident: string; const Value: Int64); overload;
+    procedure WriteInt64(const Ident: string; const Value: Int64); overload;
+    function ReadInt64(const Section, Ident: string; const Default: Int64): Int64; overload;
+    function ReadInt64(const Ident: string; const Default: Int64): Int64; overload;
     {$ENDIF}
-    procedure WriteDotFloat(const Section, Ident: string; Value: Double);
-    function ReadDotFloat(const Section, Name: string; Default: Double): Double;
+
+    procedure WriteDotFloat(const Section, Ident: string; const Value: Double); overload;
+    procedure WriteDotFloat(const Ident: string; const Value: Double); overload;
+    function ReadDotFloat(const Section, Ident: string; const Default: Double): Double; overload;
+    function ReadDotFloat(const Ident: string; const Default: Double): Double; overload;
+
+
 
     property LeftStringBound: string read FLeftStringBound write SetLeftStringBound;
     property RightStringBound: string read FRightStringBound write SetRightStringBound;
+    property CurrentSection: string read GetCurrentSection write SetCurrentSection;
   end;
   {$endregion}
 
@@ -70,11 +106,31 @@ begin
   inherited Create(AFileName, AEncoding);
   FLeftStringBound := '[';
   FRightStringBound := ']';
+  FCurrentSection := 'MAIN';
 end;
+
+
+procedure TJppMemIniFile.CheckCurrentSection;
+begin
+  if Trim(FCurrentSection) = '' then FCurrentSection := DEFAULT_SECTION;
+end;
+
+function TJppMemIniFile.GetCurrentSection: string;
+begin
+  CheckCurrentSection;
+  Result := FCurrentSection;
+end;
+
+
 
 procedure TJppMemIniFile.WriteBoundString(const Section, Ident, Value: string);
 begin
   WriteString(Section, Ident, FLeftStringBound + Value + FRightStringBound);
+end;
+
+procedure TJppMemIniFile.WriteBoundString(const Ident, Value: string);
+begin
+  WriteBoundString(CurrentSection, Ident, Value);
 end;
 
 function TJppMemIniFile.ReadBoundString(const Section, Ident, Default: string): string;
@@ -82,7 +138,14 @@ begin
   Result := TrimBounds(ReadString(Section, Ident, Default), FLeftStringBound, FRightStringBound);
 end;
 
-function TJppMemIniFile.ReadColor(const Section: string; const Ident: string; Default: TColor): TColor;
+function TJppMemIniFile.ReadBoundString(const Ident, Default: string): string;
+begin
+  Result := ReadBoundString(CurrentSection, Ident, Default);
+end;
+
+
+
+function TJppMemIniFile.ReadColor(const Section: string; const Ident: string; const Default: TColor): TColor;
 var
   sColor: string;
   xColor: integer;
@@ -98,16 +161,36 @@ begin
   Result := xColor;
 end;
 
-procedure TJppMemIniFile.WriteColor(const Section, Ident: string; Color: TColor);
+function TJppMemIniFile.ReadColor(const Ident: string; const Default: TColor): TColor;
+begin
+  Result := ReadColor(CurrentSection, Ident, Default);
+end;
+
+
+procedure TJppMemIniFile.WriteColor(const Section, Ident: string; const Color: TColor);
 begin
   WriteString(Section, Ident, ColorToString(Color));
 end;
 
-function TJppMemIniFile.ReadDotFloat(const Section, Name: string; Default: Double): Double;
+
+
+procedure TJppMemIniFile.WriteColor(const Ident: string; const Color: TColor);
+begin
+  WriteColor(CurrentSection, Ident, Color);
+end;
+
+
+
+function TJppMemIniFile.ReadDotFloat(const Ident: string; const Default: Double): Double;
+begin
+  Result := ReadDotFloat(CurrentSection, Ident, Default);
+end;
+
+function TJppMemIniFile.ReadDotFloat(const Section, Ident: string; const Default: Double): Double;
 var
   FloatStr: string;
 begin
-  FloatStr := ReadString(Section, Name, '');
+  FloatStr := ReadString(Section, Ident, '');
   FloatStr := StringReplace(FloatStr, '.', FormatSettings.DecimalSeparator, []);
   Result := Default;
   if FloatStr <> '' then
@@ -122,7 +205,8 @@ begin
 //  end;
 end;
 
-procedure TJppMemIniFile.WriteDotFloat(const Section, Ident: string; Value: Double);
+
+procedure TJppMemIniFile.WriteDotFloat(const Section, Ident: string; const Value: Double);
 var
   s: string;
 begin
@@ -131,15 +215,88 @@ begin
   WriteString(Section, Ident, s);
 end;
 
+procedure TJppMemIniFile.WriteDotFloat(const Ident: string; const Value: Double);
+begin
+  WriteDotFloat(CurrentSection, Ident, Value);
+end;
+
+
 procedure TJppMemIniFile.WriteFontStyle(const Section: string; const Ident: string; FontStyles: TFontStyles);
 begin
   WriteString(Section, Ident, FontStylesToStr(FontStyles));
 end;
 
+procedure TJppMemIniFile.WriteFontStyle(const Ident: string; FontStyles: TFontStyles);
+begin
+  WriteFontStyle(CurrentSection, Ident, FontStyles);
+end;
+
+function TJppMemIniFile.ReadFontStyle(const Section: string; const Ident: string; Default: TFontStyles): TFontStyles;
+var
+  s: string;
+begin
+  s := FontStylesToStr(Default);
+  s := ReadString(Section, Ident, s);
+  Result := StrToFontStyles(s);
+end;
+
+function TJppMemIniFile.ReadFontStyle(const Ident: string; Default: TFontStyles): TFontStyles;
+begin
+  Result := ReadFontStyle(CurrentSection, Ident, Default);
+end;
+
+
+procedure TJppMemIniFile.WriteHtmlColor(const Section, Ident: string; const AColor: TColor);
+begin
+  WriteString(Section, Ident, ColorToHtmlColorStr(AColor, '#', True));
+end;
+
+procedure TJppMemIniFile.WriteHtmlColor(const Ident: string; const AColor: TColor);
+begin
+  WriteHtmlColor(CurrentSection, Ident, AColor);
+end;
+
+
+function TJppMemIniFile.ReadHtmlColor(const Section, Ident: string; const Default: TColor): TColor;
+var
+  s: string;
+begin
+  s := ReadString(Section, Ident, '');
+  if s = '' then Exit(Default);
+  if not TryHtmlStrToColor(s, Result) then Result := Default;
+end;
+
+function TJppMemIniFile.ReadHtmlColor(const Ident: string; const Default: TColor): TColor;
+begin
+  Result := ReadHtmlColor(CurrentSection, Ident, Default);
+end;
+
+function TJppMemIniFile.ReadIntegerInRange(const Section, Ident: string; const Default, Min, Max: integer): integer;
+var
+  x: integer;
+begin
+  x := ReadInteger(Section, Ident, Default);
+  Result := GetIntInRange(x, Min, Max);
+end;
+
+function TJppMemIniFile.ReadIntegerInRange(const Ident: string; const Default, Min, Max: integer): integer;
+begin
+  Result := ReadIntegerInRange(CurrentSection, Ident, Default, Min, Max);
+end;
+
+
+
+
+
 {$IFDEF DCC}
 procedure TJppMemIniFile.WriteInt64(const Section, Ident: string; const Value: Int64);
 begin
   WriteString(Section, Ident, IntToStr(Value));
+end;
+
+procedure TJppMemIniFile.WriteInt64(const Ident: string; const Value: Int64);
+begin
+  WriteInt64(CurrentSection, Ident, Value);
 end;
 
 function TJppMemIniFile.ReadInt64(const Section, Ident: string; const Default: Int64): Int64;
@@ -150,16 +307,18 @@ begin
   s := ReadString(Section, Ident, 'ERR');
   if TryStrToInt64(s, x) then Result := x else Result := Default;
 end;
+
+function TJppMemIniFile.ReadInt64(const Ident: string; const Default: Int64): Int64;
+begin
+  Result := ReadInt64(CurrentSection, Ident, Default);
+end;
 {$ENDIF}
 
-function TJppMemIniFile.ReadFontStyle(const Section: string; const Ident: string; Default: TFontStyles): TFontStyles;
-var
-  s: string;
-begin
-  s := FontStylesToStr(Default);
-  s := ReadString(Section, Ident, s);
-  Result := StrToFontStyles(s);
-end;
+
+
+
+
+
 
 procedure TJppMemIniFile.WriteStrings(const Section: string; Items: TStrings {$IFDEF DCC}; Compress: Boolean{$ENDIF});
 var
@@ -210,6 +369,8 @@ begin
 
 
 end;
+
+
 
 procedure TJppMemIniFile.ReadStrings(const Section: string; Items: TStrings {$IFDEF DCC}; ItemsCompressed: Boolean{$ENDIF});
 var
@@ -302,6 +463,11 @@ begin
 end;
 
 
+
+procedure TJppMemIniFile.SetCurrentSection(const Value: string);
+begin
+  FCurrentSection := Value;
+end;
 
 procedure TJppMemIniFile.SetLeftStringBound(const Value: string);
 begin
